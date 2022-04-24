5e54b2346fb24a73a4cb586f48fb4216aa071f3a
Dmitri <dmitribeluy@gmail.com>
2022-04-24 14:34:59 +0300

    pervyi commit

---
+++ nazvanieperemennoi.js
@@ -0,0 +1,22 @@
+const vehicleBodyWidth = 5000;
+const vehicleBodyLength = 4000;
+
+////
+
+
+console. log("ширина кузова автомобиля:"  + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+
+// ['vruru', 'ecrvrev', 'asfasff'].map(a => )
+
+// Requset
+// data
+// Response
+
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+let COLOR_RED = '#F00';
+
